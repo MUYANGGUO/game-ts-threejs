47decad7a80df252704dc1f6f78b6dd2be7daff5
MUYANGGUO <muyangguo@gmail.com>
2020-05-08 17:42:10 -0400

    fixed a structure problem

--- src/components/Hello.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export interface HelloProps { compiler: string; framework: string; }
-
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}! a shit work from 刀哥和杯哥 。</h1>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { App } from "./components/App";
+
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <App compiler="TypeScript" framework="React" />,
+    rootElement
 );
